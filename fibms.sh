623d6e6cfcb8e5ec1ca9db41bb76b0d38211f8bf
Rube <353371737@qq.com>
2016-04-28 23:49:16 +0800

    update start script

--- fibms.sh
+++ fibms.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-function exist()
-{
-	local array=($1)
-	result=$(echo ${array[*]} |grep -q $2 && echo 1 || echo 0)
-	return ${result}
-}
+cd center && npm install
+cd ../
+rm -rf nohup.out
 
 sys=("center" "core" "producer" "consumer")
 
 for sysname in ${sys[@]} 
 do
-	pid=$(ps -e | grep "fibjs ${sysname}/app.js")
-	exist "${pid}" "grep"
-	rs=$?
-	if [ $rs == 0 ]
+	pid=($(ps -ef | grep "fibjs ${sysname}/app.js"))
+	if [ "${pid[7]}" != "grep" ]
 	then
-		pidarr=($pid)
-		kill ${pidarr[0]}
+		echo "kill ${pid[1]}"
+		kill ${pid[1]}
 	fi
-	echo $sysname
+	sleep 5
+	echo "start ${sysname}"
 	nohup fibjs ${sysname}/app.js &
 	disown %1
-	sleep 2
 done
