512585bf78becf154d25eb0008ab0c861982501e
Rube <353371737@qq.com>
2016-04-28 14:44:58 +0800

    定时，循环消息 example

--- examples/producer-nodejs.js
+++ examples/producer-nodejs.js
@@ -10,6 +10,7 @@ let message;
 // message.setParams('hellogroup', 'yes');
 // Producer.sendMessage(message);
 
+//综合
 message = Producer.createMessage('all');
 message.setType(Producer.MESSAGE_GROUP);
 message.setProperty(Producer.SCHEDULED_DEALY, 10002);
@@ -18,11 +19,22 @@ message.setProperty(Producer.SCHEDULED_REPEAT, 6);
 message.setParams('hello all', 'ok');
 Producer.sendMessage(message);
 
+//循环消息
+message = Producer.createMessage('all');
+message.setType(Producer.MESSAGE_GROUP);
+message.setProperty(Producer.SCHEDULED_DEALY, 1000);
+message.setProperty(Producer.SCHEDULED_PERIOD, 2 * 1000);
+message.setProperty(Producer.SCHEDULED_REPEAT, 0);
+message.setParams('hello all-----', 'ok');
+Producer.sendMessage(message);
+
+//延迟消息
 message = Producer.createMessage('dealy');
 message.setProperty(Producer.SCHEDULED_DEALY, 10 * 1000);
 message.setParams('hello dealy', 'ok');
 Producer.sendMessage(message);
 
+//定时消息
 message = Producer.createMessage('period');
 message.setProperty(Producer.SCHEDULED_PERIOD, 6 * 1000);
 message.setProperty(Producer.SCHEDULED_REPEAT, 6);
